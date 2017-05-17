513a0749297d79d5933e458acb8e62302ad402fc
Bennett Roesch <meta@spyrosoft.com>
2017-05-16 21:08:55 -0700

    Remove redundant string test in strictFloat

--- utilities.js
+++ utilities.js
@@ -156,9 +156,6 @@ var Utilities = {
 	
 	strictFloat : function( input ) {
 		if ( isFinite( input ) ) { return parseFloat(input); }
-		if ( typeof input === 'string' && input === parseFloat( input ).toString() ) {
-			return parseFloat( input );
-		}
 		return undefined;
 	},
 	
